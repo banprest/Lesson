55243fc4488da6f1f7a8020c33c6008da13de491
banpresto <sexyteemo@gmail.com>
2020-10-24 19:45:24 +0300

    Lesson 2

--- entered_date.rb
+++ entered_date.rb
@@ -4,9 +4,9 @@ day = gets.chomp.to_i
 months = gets.chomp.to_i
 year = gets.chomp.to_i
 
-ldays = [31,29,31,30,31,30,31,31,30,31,30,31]
+ldays = [31,28,31,30,31,30,31,31,30,31,30,31]
 if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-  ldays[1] = 28
+  ldays[1] = 29
 end
 puts ldays.take(months).sum - day
 
