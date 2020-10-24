ab08e1a70be17e66c1e870486c8b21d67b1e3e51
banpresto <sexyteemo@gmail.com>
2020-10-24 14:16:22 +0300

    lesson 2

--- entered_date.rb
+++ entered_date.rb
@@ -1,38 +1,12 @@
 puts 'Введите дату в формате dd.mm.yyyy'
 
 day = gets.chomp.to_i
-months = gets.chomp
+months = gets.chomp.to_i
 year = gets.chomp.to_i
 
 ldays = [31,29,31,30,31,30,31,31,30,31,30,31]
-not_ldays = [30,28,31,30,31,30,31,31,30,31,30,31]
-count = 0
-lep = 31
-nlep = 31
-leap_days = [31]
-not_leap_days= [31]
-loop do
-  lep = lep + ldays[count + 1]
-  nlep = nlep + not_ldays[count + 1]
-  leap_days << lep
-  not_leap_days << nlep
-  count += 1
-  break if count == 11
-end
-
-leap = Hash[[('01'..'12').to_a, leap_days].transpose]
-not_leap = Hash[[('01'..'12').to_a, not_leap_days].transpose]
-
 if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-  leap.each do |month, days|
-    if months == month
-      puts days - day
-    end
-  end
-else
-  not_leap.each do |month, days|
-    if months == month
-      puts days - day
-    end
-  end
+  ldays[1] = 28
 end
+puts ldays.take(months).sum - day
+
